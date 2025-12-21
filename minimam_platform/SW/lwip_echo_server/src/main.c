117b4bb90b7950b35ab7be1fdac32928e39b52b4
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2025-12-21 09:47:52 +0900

    RTC@I2Cの読み出しと,SD Card読み書きを追加します.

--- minimam_platform/SW/lwip_echo_server/src/main.c
+++ minimam_platform/SW/lwip_echo_server/src/main.c
@@ -1,240 +1,159 @@
-/*
- * Copyright (C) 2009 - 2022 Xilinx, Inc.
- * Copyright (C) 2022 - 2024 Advanced Micro Devices, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGE.
- *
- */
-
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "lwip/def.h"
-#include "lwip/netif.h"
 #include "xparameters.h"
+#include "ff.h"
+#include "xdevcfg.h"
+#include "xiicps.h"
 
-#include "netif/xadapter.h"
-
-#include "platform.h"
-#include "platform_config.h"
-#if defined (__arm__) || defined(__aarch64__)
 #include "xil_printf.h"
-#endif
+#include "echo_server.h"
 
-#include "rtl8211f_phy.h"
-#include "netif/ethernet.h"
-#include "lwip/tcp.h"
-#include "xil_cache.h"
+#define I2C_SCLK_RATE (400000) // 400KHz
+#define I2C_RTC_SLAVE_ADDRESS (0x51)
 
-#if LWIP_IPV6==1
-#include "lwip/ip.h"
-#else
-#if LWIP_DHCP==1
-#include "lwip/dhcp.h"
-#endif
-#endif
+XIicPs s_I2c0Instance;
+struct time {
+    uint8_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t min;
+    uint8_t sec;
+};
 
-#include "xemac_adapter/xemac_adapter.h"
+static int32_t read_rtc(struct time *now){
 
-/* defined by each RAW mode application */
-void print_app_header();
-int start_application();
-int transfer_data();
-void tcp_fasttmr(void);
-void tcp_slowtmr(void);
+    XIicPs_Config *pI2c0Cfg = NULL;
+    uint8_t buf[10] = { 0 };
+    int32_t status;
+    
+    pI2c0Cfg = XIicPs_LookupConfig(XPAR_XIICPS_0_BASEADDR);
+    if(pI2c0Cfg == NULL){
+        return -1;
+    }
 
-/* missing declaration in lwIP */
-void lwip_init();
+    status = XIicPs_CfgInitialize(&s_I2c0Instance, pI2c0Cfg, pI2c0Cfg->BaseAddress); 
+    if(status != XST_SUCCESS){
+        return -1;
+    }
 
-#if LWIP_IPV6==0
-#if LWIP_DHCP==1
-extern volatile int dhcp_timoutcntr;
-err_t dhcp_start(struct netif *netif);
-#endif
-#endif
+    status = XIicPs_SelfTest(&s_I2c0Instance);
+    if(status != XST_SUCCESS){
+        return -1;
+    }
 
-extern volatile int TcpFastTmrFlag;
-extern volatile int TcpSlowTmrFlag;
-static struct netif server_netif;
-struct netif *echo_netif;
+    status = XIicPs_SetSClk(&s_I2c0Instance, I2C_SCLK_RATE);
+    if(status != XST_SUCCESS){
+        return -1;
+    }
 
-#if LWIP_IPV6==1
-void print_ip6(char *msg, ip_addr_t *ip)
-{
-	print(msg);
-	xil_printf(" %x:%x:%x:%x:%x:%x:%x:%x\n\r",
-		   IP6_ADDR_BLOCK1(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK2(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK3(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK4(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK5(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK6(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK7(&ip->u_addr.ip6),
-		   IP6_ADDR_BLOCK8(&ip->u_addr.ip6));
+    buf[0] = 0x02;
+    status = XIicPs_MasterSendPolled(&s_I2c0Instance, &buf[0], 1, I2C_RTC_SLAVE_ADDRESS);
+    if(status != XST_SUCCESS){
+        return -1;
+    }
+    /* Wait until bus is idle to start another transfer */
+    while (XIicPs_BusIsBusy(&s_I2c0Instance));
 
+    status = XIicPs_MasterRecvPolled(&s_I2c0Instance, &buf[0], 7, I2C_RTC_SLAVE_ADDRESS);
+    if(status != XST_SUCCESS){
+        return -1;
+    }
+    
+    now->sec   = ((buf[0] & 0x70) >> 4)*10 + (buf[0] & 0x0F);
+    now->min   = ((buf[1] & 0x70) >> 4)*10 + (buf[1] & 0x0F);
+    now->hour  = ((buf[2] & 0x70) >> 4)*10 + (buf[2] & 0x0F);
+    now->day   = ((buf[3] & 0x70) >> 4)*10 + (buf[3] & 0x0F);
+    now->month = ((buf[5] & 0x10) >> 4)*10 + (buf[5] & 0x0F);
+    now->year  = ((buf[6] & 0xF0) >> 4)*10 + (buf[6] & 0x0F);
+    xil_printf("now, %d:%d:%d:%d:%d:%d\r\n", now->year, now->month, now->day, now->hour, now->min, now->sec);
+        
+    return 0;
 }
-#else
-void print_ip(char *msg, ip_addr_t *ip)
-{
-	print(msg);
-	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip),
-		   ip4_addr3(ip), ip4_addr4(ip));
+
+static const TCHAR* s_pSDDev="0:/";
+
+static int init_fatds(FATFS *pFatfs){
+
+    FRESULT rc;
+    
+    rc = f_mount(pFatfs, s_pSDDev, 0);
+    if(rc != FR_OK){
+        printf("failed to mount sd: %d\n", rc);
+        return rc;
+    }
+    
+    return FR_OK;
 }
 
-void print_ip_settings(ip_addr_t *ip, ip_addr_t *mask, ip_addr_t *gw)
-{
+int SDFfunctuionTest(struct time *now){
+    FATFS fatfs;
+    FRESULT rc;
+    char test_buf[256] = {'\0'};
+    int write_size;
+    
+    rc = init_fatds(&fatfs);
+    if(rc != FR_OK){
+        return XST_FAILURE;
+    }
+    
+    {
+        DIR dir;
+        FILINFO fno;
+        rc = f_opendir(&dir, s_pSDDev);
+        if(rc != FR_OK){
+            goto error_exit;
+        }
+        
+        while(FR_OK==f_readdir(&dir, &fno)){
+            if(fno.fname[0] == '\0'){
+                break;
+            }
+            printf("%s\n",fno.fname);	
+        }
+        f_closedir(&dir);
+    }
+    
+    write_size = snprintf(test_buf, sizeof(test_buf)-1, "%d/%d/%d, %d:%d:%d", 2000+now->year, now->month, now->day, now->hour, now->min, now->sec);
 
-	print_ip("Board IP: ", ip);
-	print_ip("Netmask : ", mask);
-	print_ip("Gateway : ", gw);
+    {
+        FIL fil;
+        UINT bw;
+        f_open(&fil, "nowtime.txt", FA_CREATE_ALWAYS|FA_WRITE);
+        f_lseek(&fil, 0);
+        f_write(&fil, &test_buf[0],  write_size, &bw);
+        f_close(&fil);
+    }
+    memset(test_buf, 0x00, sizeof(test_buf));
+    
+    {
+        FIL fil;
+        UINT bw;
+        rc = f_open(&fil, "nowtime.txt", FA_READ);
+        if(rc == FR_OK){
+            f_lseek(&fil, 0);
+            f_read(&fil, &test_buf[0], write_size, &bw);
+            f_close(&fil);
+            xil_printf("%s\r\n", &test_buf[0]);
+        } else {
+            xil_printf("failed to open nowtime.txt\r\n");
+        }
+    }
+
+    
+error_exit:
+
+    f_unmount(s_pSDDev);
+    return XST_SUCCESS;	
 }
-#endif
-
-/* the mac address of the board. this should be unique per board */
-#define MAC_ADDRESS { 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 }
-const static uint32_t default_ipv4_addr = LWIP_MAKEU32(192, 168, 10, 91);
-const static uint32_t default_ipv4_netmask = LWIP_MAKEU32(255, 255, 255, 0);
-const static uint32_t default_ipv4_gw = LWIP_MAKEU32(192, 168, 10, 1);
 
 int main()
 {
-    struct xemac_adapter_context pseth_context = {
-        .netif = NULL,
-        .phy = {
-            .phyaddr = 0,
-            .phy_setup = rtl8211f_phy_setup,
-            .phy_link_speed = rtl8211f_phy_link_speed
-        },
-        .macaddr = MAC_ADDRESS,
-        .mac_regaddr = (void *)PLATFORM_EMAC_BASEADDR
-    };
-    
-#if LWIP_IPV6==0
-    ip_addr_t ipaddr, netmask, gw;
-    
-#endif
-    echo_netif = &server_netif;
-    
-    /* Define this board specific macro in order perform PHY reset on ZCU102 */
-    
-    init_platform();
-    
-#if LWIP_IPV6==0
-#if LWIP_DHCP==1
-    ipaddr.addr = 0;
-    gw.addr = 0;
-    netmask.addr = 0;
-#else
-    /* initialize IP addresses to be used */
-    ip4_addr_set_u32(&ipaddr,  PP_HTONL(default_ipv4_addr));
-    ip4_addr_set_u32(&netmask, PP_HTONL(default_ipv4_netmask));
-    ip4_addr_set_u32(&gw,      PP_HTONL(default_ipv4_gw));
-#endif
-#endif
-    print_app_header();
-    
-    lwip_init();
-    
-#if (LWIP_IPV6 == 0)
-    /* Add network interface to the netif_list, and set it as default */
-    echo_netif = netif_add(echo_netif, &ipaddr, &netmask, &gw, &pseth_context, xemac_adapter_init, ethernet_input);
-#else
-    /* Add network interface to the netif_list, and set it as default */
-    if (!xemac_add(echo_netif, NULL, NULL, NULL, mac_ethernet_address,
-                   PLATFORM_EMAC_BASEADDR)) {
-        xil_printf("Error adding N/W interface\n\r");
-        return -1;
-    }
-    echo_netif->ip6_autoconfig_enabled = 1;
-        
-    netif_create_ip6_linklocal_address(echo_netif, 1);
-    netif_ip6_addr_set_state(echo_netif, 0, IP6_ADDR_VALID);
-    
-    print_ip6("\n\rBoard IPv6 address ", &echo_netif->ip6_addr[0].u_addr.ip6);
-    
-#endif
-    netif_set_default(echo_netif);
-    
-#ifndef SDT
-    /* now enable interrupts */
-    platform_enable_interrupts();
-#endif
-    
-    /* specify that the network if is up */
-    netif_set_up(echo_netif);
-    
-#if (LWIP_IPV6 == 0)
-#if (LWIP_DHCP==1)
-    /* Create a new DHCP client for this interface.
-     * Note: you must call dhcp_fine_tmr() and dhcp_coarse_tmr() atxbg
-     * the predefined regular intervals after starting the client.
-     */
-    dhcp_start(echo_netif);
-    dhcp_timoutcntr = 240;
-    
-    while (((echo_netif->ip_addr.addr) == 0) && (dhcp_timoutcntr > 0)) {
-      // xemacif_input(echo_netif);
-        xemac_adapter_input(echo_netif);
-    }
-    
-    if (dhcp_timoutcntr <= 0) {
-      if ((echo_netif->ip_addr.addr) == 0) {
-            ip4_addr_set_u32(&ipaddr,  PP_HTONL(default_ipv4_addr));
-            ip4_addr_set_u32(&netmask, PP_HTONL(default_ipv4_netmask));
-            ip4_addr_set_u32(&gw,      PP_HTONL(default_ipv4_gw));                    
-            xil_printf("DHCP Timeout\r\n");
-            xil_printf("Configuring default IP of %d.%d.%d.%d.\r\n",
-                       ip4_addr1_val(ipaddr), ip4_addr2_val(ipaddr), ip4_addr3_val(ipaddr), ip4_addr4_val(ipaddr));
-        }
-    }
-    
-    ipaddr.addr = echo_netif->ip_addr.addr;
-    gw.addr = echo_netif->gw.addr;
-    netmask.addr = echo_netif->netmask.addr;
-#endif
-    
-    print_ip_settings(&ipaddr, &netmask, &gw);
-    
-#endif
-    /* start the application (web server, rxtest, txtest, etc..) */
-    start_application();
-
-    /* receive and process packets */
-    while (1) {
-        if (TcpFastTmrFlag) {
-            tcp_fasttmr();
-            TcpFastTmrFlag = 0;
-        }
-        if (TcpSlowTmrFlag) {
-            tcp_slowtmr();
-            TcpSlowTmrFlag = 0;
-        }
-        xemac_adapter_input(echo_netif);
-        transfer_data();
-    }
-
-    /* never reached */
-    cleanup_platform();
-    
+    struct time now = { 0 };
+    read_rtc(&now);
+    SDFfunctuionTest(&now);
+    echo_server();    
     return 0;
 }
