46f5f16a2e4230bf5bb3ec782de7363deeea6d23
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2026-01-31 08:46:02 +0000

    changes rootfs image to rootfs.cpio.gz.

--- buildroot/ramroot_boot.cmd
+++ buildroot/ramroot_boot.cmd
@@ -3,7 +3,7 @@ setenv fdt_high    0x30000000
 
 fatload mmc 0:1 0x02000000 zImage
 fatload mmc 0:1 0x01000000 system.dtb
-fatload mmc 0:1 0x04000000 rootfs.cpio
+fatload mmc 0:1 0x04000000 rootfs.cpio.gz
 setenv rdsize ${filesize}
 
 setenv bootargs 'console=ttyPS0,115200 earlycon=cdns,0xe0000000,115200 loglevel=8 ignore_loglevel'
